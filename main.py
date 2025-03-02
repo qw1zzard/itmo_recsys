5f3ce063834eb7dc6346523ee0602542b1e1bad1
Igor Pivnev <iq.pivnev@gmail.com>
2025-03-02 21:55:40 +0300

    Fix exception types for workflow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 import uvicorn
 
 from service.api.app import create_app
@@ -9,7 +10,6 @@ app = create_app(config)
 
 
 if __name__ == "__main__":
-
     host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8080"))
 
